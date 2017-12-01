258704ade1694445876e5f8a79f00ce452b6c92e
Surprise <turtrozz@gmail.com>
2017-11-30 17:40:17 -0800

    First menu tab added

--- maritoXitsonga.py
+++ maritoXitsonga.py
@@ -7,6 +7,15 @@ from tkinter import *
 
 
 
+
+
+#============The Widgets related code====================#
+
+
+
+		
+
+
 #=========The app python main function implementation class==============#
 
 class Main:
@@ -15,7 +24,36 @@ class Main:
 
 	def main():
 
-		print("Hello World")
+		mainAppWindow = Tk()
+
+		
+		mainAppWindow.resizable(width=True,height=True)
+		mainAppWindow.minsize(width=600,height=500)
+		mainAppWindow.maxsize(width=1400,height=1400)
+		mainAppWindow.title('MaritoXitsonga')
+		mainAppWindow.iconbitmap("favicon.ico")
+
+		menuBar = Menu(mainAppWindow)
+
+		m_bookmarkTab = Menu(menuBar,tearoff = 0)
+
+		m_bookmarkTab.add_command(label="Surprise")
+		m_bookmarkTab.add_command(label="Shadrack")
+		m_bookmarkTab.add_command(label="Happy")
+		m_bookmarkTab.add_separator()
+		m_bookmarkTab.add_command(label="Ntiyiso")
+		m_bookmarkTab.add_command(label="Sophy")
+
+		menuBar.add_cascade(label="Bookmark",menu = m_bookmarkTab)
+
+		mainAppWindow.config(menu=menuBar)
+
+
+		mainAppWindow.mainloop()
+
+
+
+
 
 
 
@@ -38,7 +76,11 @@ class App:
 
 
 
-#============The Widgets related code====================#
+
+
+
+
+
 
 	
 
